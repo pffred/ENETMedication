1436c7e925b32b262cec6eb917561ec158ed1c19
demonlition <demonlition00@gmail.com>
2015-03-17 19:29:28 +1100

    added moar tests
    basic tests for more basic functionality

--- ENETTests/InformationSwapTests.cs
+++ ENETTests/InformationSwapTests.cs
@@ -13,6 +13,58 @@ namespace ENETTests
     public class InformationSwapTests
     {
 
+EnetSys _EnetSys;
+
+            public TestContext TestContext { get; set; }
+
+            [TestInitialize]
+            public void Setup()
+            {
+                //sets up base program
+                _EnetSys = new EnetSys();
+            }
+
+
+            [TestMethod]
+            public void User_OnSetup_Exists()
+            {
+                //checks that a user exists
+                Assert.IsNotNull(_EnetSys._employee);   
+            }
+
+
+            [TestMethod]
+            public void Location_OnSetup_Exists()
+            {
+                //checks that a location exists
+                Assert.IsNotNull(_EnetSys._location);
+            }
+
+
+            [TestMethod]
+            public void Package_OnSetup_Exists()
+            {
+                //checks that a package exists
+                Assert.IsNotNull(_EnetSys._package);
+            }
+
+
+            [TestMethod]
+            public void User_OnLogin_SuccessfulLogin()
+            {
+                //checks that a user can login
+                Assert.IsTrue(_EnetSys._employee.login("pass"));
+            }
+
+
+            [TestMethod]
+            public void User_OnLogin_HasAttributes()
+            {
+                //checks that a user has stats
+                Assert.IsNotNull(_EnetSys._employee.Fullname);
+                Assert.IsNotNull(_EnetSys._employee.EmailAddress);
+            }
+
         [TestMethod]
         public void DistributionCentre_PackageValueOfPackages()
         {
